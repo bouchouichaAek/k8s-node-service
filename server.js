f80c2ef020861da378f37eca9c38999fe0058886
Abdelkader Bouchouicha <bouchouichaaek97@gmail.com>
2024-07-27 19:23:02 +0100

    first commit

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send(`Hello, World! I am Pod ${os.hostname()}`);
+  res.send(`Hello, World! I am Pod ${os.hostname()} Version 1.2.0`);
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
